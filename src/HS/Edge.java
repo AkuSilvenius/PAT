b1622b12bcc18c833930b8641ab5747948ba62a2
AkuSilvenius <akus@student.uef.fi>
2016-08-01 23:53:03 +0300

    Merge conflict korjaus

--- src/HS/Edge.java
+++ src/HS/Edge.java
@@ -1,84 +1,10 @@
-<<<<<<< HEAD
-package HS;
-
-import java.util.LinkedList;
-
-public class Edge<E> implements EdgeIn<E> {
-	
-	Vertex<E> startPoint;
-	Vertex<E> endPoint;
-	float weight;
-	int color;
-
-	public Edge() {
-		this.startPoint = null;
-		this.endPoint = null;
-		this.weight = Float.NaN;
-		this.color = HSCol.NaN;
-	}
-	
-	public Edge(Vertex<E> endpoint) {
-		this.startPoint = null;
-		this.endPoint = endpoint;
-		this.weight = Float.NaN;
-		this.color = HSCol.NaN;
-	}
-	
-	public Edge(Vertex<E> endpoint, float w) {
-		this.startPoint = null;
-		this.endPoint = endpoint;
-		this.weight = w;
-		this.color = HSCol.NaN;
-	}
-	
-	public Edge(Vertex<E> endpoint, int clr) {
-		this.startPoint = null;
-		this.endPoint = endpoint;
-		this.weight = Float.NaN;
-		this.color = clr;
-	}
-	
-	public Edge(Vertex<E> endpoint, float w, int clr) {
-		this.startPoint = null;
-		this.endPoint = endpoint;
-		this.weight = w;
-		this.color = clr;
-	}
-
-	@Override
-	public void setWeight(int w) {
-		this.weight = w;
-	}
-
-	@Override
-	public float getWeight() {
-		return this.weight;
-	}
-
-	@Override
-	public Vertex<E> getEndPoint() {
-		return this.endPoint;
-	}
-
-	@Override
-	public Vertex<E> getEndPoint(Vertex<E> v) {
-		if (!v.equals(this.startPoint)) return null;
-		if (v.equals(this.startPoint)) return this.endPoint;
-		return this.startPoint;
-	}
-
-	@Override
-	public Vertex<E> getStartPoint() {
-		return this.startPoint;
-	}
-=======
 package HS;
 
 import java.util.LinkedList;
 
 public class Edge<E> {
-	
-	
+
+
 	Vertex<E> startPoint;
 	Vertex<E> endPoint;
 	float weight;
@@ -90,28 +16,28 @@ public class Edge<E> {
 		this.weight = Float.NaN;
 		this.color = HSCol.NaN;
 	}
-	
+
 	public Edge(Vertex<E> endpoint) {
 		this.startPoint = null;
 		this.endPoint = endpoint;
 		this.weight = Float.NaN;
 		this.color = HSCol.NaN;
 	}
-	
+
 	public Edge(Vertex<E> endpoint, float w) {
 		this.startPoint = null;
 		this.endPoint = endpoint;
 		this.weight = w;
 		this.color = HSCol.NaN;
 	}
-	
+
 	public Edge(Vertex<E> endpoint, int clr) {
 		this.startPoint = null;
 		this.endPoint = endpoint;
 		this.weight = Float.NaN;
 		this.color = clr;
 	}
-	
+
 	public Edge(Vertex<E> endpoint, float w, int clr) {
 		this.startPoint = null;
 		this.endPoint = endpoint;
@@ -140,5 +66,5 @@ public class Edge<E> {
 	public Vertex<E> getStartPoint() {
 		return this.startPoint;
 	}
->>>>>>> origin/master
+	
 }
